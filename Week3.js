d75f5789cba7e01ac4f28e5087ab52c895377c58
Fernando Garcia <fgarcia@youthbuildatx.org>
2022-01-20 15:32:28 -0600

    Finished easy/medium challenges

--- Week3.js
+++ Week3.js
@@ -9,7 +9,24 @@ var swim = exercise('swimming');
 console.log(swim()); // prints "Today's exercise: swimming"
 
 function exercise(exercise){
-    return (exercise) => {
-        return "Todays exercise:" + exercise
-    }
+        return () => `Today’s exercise: ${exercise}`
 }
+
+
+// Medium
+
+//  Sets sharePizza to cutPizzaSlices
+var sharePizza = cutPizzaSlices(8);
+
+function cutPizzaSlices(slices) {
+        return (people) => `Each person gets ${slices/people} slices of pizza` 
+        
+}
+
+console.log(sharePizza(2));                                                                                                                                                                    
+
+  // prints "Each person gets 4.00 slices of pizza"
+
+console.log(sharePizza(3)); 
+
+  // prints "Each person gets 2.67 slices of pizza"
